c1ac8671b64c65a215dadc76a7f32cbcfd82dc4a
zhangliang <zhangliang@bytedance.com>
2016-04-20 15:51:40 +0800

    test html

--- main.py
+++ main.py
@@ -1,46 +1,28 @@
 #!/usr/bin/env python
 #coding=utf-8
 __author__ = 'Phtih0n'
-import web, os, pxfilter
+import pxfilter
+from flask import Flask
+from flask import request
+from flask import render_template
 
-urls = (
-    '.*', 'router'
-)
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    html = {
+        'originHtml': '',
+        'cleanHtml': ''
+    }
+    if request.method == 'POST' and request.form.get("content"):
+        content = request.form.get("content")
+        html['originHtml'] = content
+        parser = pxfilter.XssHtml()
+        parser.feed(content)
+        parser.close()
+        html['cleanHtml'] = parser.getHtml()
+
+    return render_template('xsshtml.html', html=html)
 
 if __name__ == "__main__":
-    app = web.application(urls, globals())
-    web.config.debug = False
-    app.run()
-
-class su:
-    def other(self):
-        raise web.seeother('main')
-
-class router(su):
-    def __init__(self):
-        self.tplData = {}
-        self.globalsTplFuncs = {}
-        web.header("X-XSS-Protection", "0")
-
-    def GET(self):
-        self.assign("html", "")
-        return self.display("xsshtml")
-
-    def POST(self):
-        html = web.input(xsscode = "").xsscode
-        parser = pxfilter.XssHtml()
-        parser.feed(html)
-        parser.close()
-        html = parser.getHtml()
-        self.assign("html", html)
-        return self.display("xsshtml")
-
-    def assign(self,key,value = ''):
-        if type(key) == dict:
-            self.tplData = dict(self.tplData, **key)
-        else:
-            self.tplData[key] = value
-
-    def display(self, tplName):
-        self.tplData['render'] = web.template.render('html', globals = self.globalsTplFuncs)
-        return getattr(self.tplData['render'], tplName)(self.tplData)
+    app.run(debug=True)
